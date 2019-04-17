7ea6e4286d97081c8a7ec6a1661fdbdc4e42f9c0
Daniel <sentret@outlook.com>
2019-04-17 10:59:23 -0300

    Use /tldr's URL extraction

--- commands/paywall.go
+++ commands/paywall.go
@@ -3,10 +3,7 @@ package commands
 import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	"regexp"
 )
-
-var url_matcher = regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
 
 //Paywall handles the "/paywall" bot command
 func Paywall(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
@@ -23,10 +20,7 @@ func appender(bot *tgbotapi.BotAPI, update tgbotapi.Update, base_url string) {
 	if message.ReplyToMessage == nil {
 		return
 	}
-
-	url_matcher.Longest()
-	link := url_matcher.FindString(message.ReplyToMessage.Text)
-
+	link := getFirstURL(message.ReplyToMessage.Text)
 	if len(link) != 0 {
 		reply := fmt.Sprintf("🏴‍☠️ [Link sem Paywall](%s/%s) 🏴‍☠️", base_url, link)
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
